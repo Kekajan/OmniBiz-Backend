dca75cf001fae085035def8af57c0c44a60ce96b
kishobigan <karunairsa.kishobigan@gmail.com>
2024-08-19 16:39:55 +0530

    Transaction time added in the all transaction list for owner and removed a slash from the line 11 in owner/urls.py

--- OmniBiz/owner/urls.py
+++ OmniBiz/owner/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('createOwner', CreateOwnerView.as_view(), name='create owner'),
     path('updateOwner/<str:user_id>/', UpdateOwnerView.as_view(), name='update owner'),
     path('get-owner/<str:user_id>', GetOwnerView.as_view(), name='get owner'),
-    path('/owner/owner-action/<str:user_id>', OwnerActionView.as_view(), name='owner action'),
+    path('owner/owner-action/<str:user_id>', OwnerActionView.as_view(), name='owner action'),
 ]
